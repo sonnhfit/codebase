05c8a8ee0ef8b01ccb3ec20da21f641a3f562c07
son nguyen <sonnhfit@gmail.com>
2019-08-15 17:03:27 +0700

    update

--- rootapp/settings.py
+++ rootapp/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'rootapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -132,6 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
