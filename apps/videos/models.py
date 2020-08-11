8fc33af214dca428297b08946fed8f86751bebdd
sonnhfit <sonnhfit@gmail.com>
2020-08-11 20:56:27 +0700

    test:

--- apps/videos/models.py
+++ apps/videos/models.py
@@ -1,1 +1,6 @@
 from django.db import models
+
+
+class FileUpload(models.Model):
+    user_key = models.CharField(max_length=100, null=True, blank=True)
+    file_up = models.FileField(upload_to='documents/%Y/%m/%d')
